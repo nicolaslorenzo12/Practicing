071ce0b9f8026309f5d85375980f6a29f31ffb45
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2021-11-26 21:48:12 +0100

    first commit

---
+++ src/ArraysV2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ArraysV2 {
+
+    public static void main(String[] args) {
+
+        StringBuilder[] suits = new StringBuilder[4];
+        suits[0] = new StringBuilder("hearts");
+        suits[1] = new StringBuilder("clubs");
+        suits[2] = new StringBuilder("diamonds");
+        suits[3] = new StringBuilder("spades");
+    }
+}
