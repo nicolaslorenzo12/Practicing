491d53174236fdde204f1153584dc009c2858fde
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2021-11-26 22:02:36 +0100

    -m 'commiting crazy things'

--- src/ArraysV2.java
+++ src/ArraysV2.java
@@ -11,6 +11,6 @@ public class ArraysV2 {
         suits[3] = new StringBuilder("spades");
 
         System.out.println("Life is shit");
+        System.out.println("I like dutch accent");
     }
 }
-d
