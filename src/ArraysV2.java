e11270474c1bd64b30f0416e6d6311319fbacaea
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2021-11-26 21:53:14 +0100

    second commit

--- src/ArraysV2.java
+++ src/ArraysV2.java
@@ -9,5 +9,7 @@ public class ArraysV2 {
         suits[1] = new StringBuilder("clubs");
         suits[2] = new StringBuilder("diamonds");
         suits[3] = new StringBuilder("spades");
+
+        System.out.println("Life is shit");
     }
 }
